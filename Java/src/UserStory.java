de3b780555958acd6bff978bc53e216f8439a46b
Dennis <dennisvu.csulb@gmail.com>
2017-05-06 01:29:56 -0700

    All methods take user input directly within

--- Java/src/UserStory.java
+++ Java/src/UserStory.java
@@ -1,18 +1,25 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
-
-
-public class UserStory{
-	
+public class UserStory
+{
 	private static Statement stmnt = null;
+	private static Scanner console = new Scanner(System.in);
 	
-	public static void createUS(String storyID, String perspective, String request, String benefit, String priority, Connection conn1){
-		try{
+	public static void createUS(Connection conn1){
+		try
+		{
+			System.out.print("Enter the user story ID: ");
+			String storyID = console.nextLine();
+			System.out.print("Enter the perspective: ");
+			String perspective = console.nextLine();
+			System.out.print("Enter the request: ");
+			String request = console.nextLine();
+			System.out.print("Enter the benefit: ");
+			String benefit = console.nextLine();
+			System.out.print("enter the priority: ");
+			String priority = console.nextLine();
+			
 			String sql = "INSERT INTO UserStory (storyID, perspective, request, benefit, priority) VALUES (?, ?, ?, ?, ?)";
 			PreparedStatement statement = conn1.prepareStatement(sql);
 			statement.setString(1, storyID);
@@ -20,53 +27,79 @@ public class UserStory{
 			statement.setString(3, request);
 			statement.setString(4, benefit);
 			statement.setString(5, priority);
+		
 			int rowCreated = statement.executeUpdate();
-			if (rowCreated > 0) {
+			if (rowCreated > 0)
 			    System.out.println("User Story created successfully.");
-			}
-		}catch(SQLException ex){
+		} catch(SQLException ex)
+		{
 			ex.printStackTrace();
 		}
 	}
-	public static void updateUS(String attr1, String attr2, String selectedID, Connection conn1){
-		try{
+	
+	public static void updateUS(Connection conn1)
+	{
+		try
+		{
+			System.out.print("Enter the first attribute: ");
+			String attr1 = console.nextLine();
+			System.out.print("Enter the second attribute: ");
+			String attr2 = console.nextLine();
+			System.out.print("Enter the selected ID: ");
+			String selectedID = console.nextLine();
+			
 			String sql = "UPDATE UserStory SET ? = ? WHERE storyID=?";
 			PreparedStatement statement = conn1.prepareStatement(sql);
 			statement.setString(1, attr1);
 			statement.setString(2, attr2);
 			statement.setString(3, selectedID);
+			
 			int rowCreated = statement.executeUpdate();
-			if (rowCreated > 0) {
+			if (rowCreated > 0)
 			    System.out.println("User Story updated successfully.");
-			}
-		}catch(SQLException ex){
+		} catch(SQLException ex)
+		{
 			ex.printStackTrace();
 		}
 	}
-	public static void deleteUS(String selectedID, Connection conn1){
-		try{
+	
+	public static void deleteUS(Connection conn1)
+	{
+		try
+		{
+			System.out.print("Enter the selected ID: ");
+			String selectedID = console.nextLine();
+			
 			String sql = "DELETE FROM UserStory WHERE storyID=?";
 			PreparedStatement statement = conn1.prepareStatement(sql);
 			statement.setString(1, selectedID);
+			
 			int rowCreated = statement.executeUpdate();
-			if (rowCreated > 0) {
+			if (rowCreated > 0)
 			    System.out.println("UserStory updated successfully.");
-			}
-		}catch(SQLException ex){
+		} catch(SQLException ex)
+		{
 			ex.printStackTrace();
 		}
 	}
-	public static void selectUS(Connection conn1){
-		try{
+	
+	public static void selectUS(Connection conn1)
+	{
+		try
+		{
 			stmnt = conn1.createStatement();
 			ResultSet results = stmnt.executeQuery("SELECT * from UserStory");
 			ResultSetMetaData rsmd = results.getMetaData();
+			
 			int numberCols = rsmd.getColumnCount();
-			for(int i=1;i<=numberCols;i++)
+			for(int i = 1; i <= numberCols; i++)
 				System.out.print(rsmd.getColumnLabel(i)+"\t\t"); //prints column names
+			
 			System.out.println("\n----------------------------------------"
 					+ "------------------------------------------------------------");
-			while(results.next()){
+			
+			while(results.next())
+			{
 				String storyID = results.getString(1);//row data
 				String perspective = results.getString(2);
 				String request = results.getString(3);
@@ -76,9 +109,9 @@ public class UserStory{
 			}
 			results.close();
 			stmnt.close();
-		}catch(SQLException ex){
+		} catch(SQLException ex)
+		{
 			ex.printStackTrace();
 		}
 	}
-
 }
